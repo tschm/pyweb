61b65710e458e58828fb8fd233b1905e8dd5ba92
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-01-22 11:50:17 +0100

    package data

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='pyweb',
     version="0.0.1",
+    #package_dir={'': "pyweb"},
     packages=find_packages(include=["pyweb*"]),
+    package_dir={'': "pyweb"},
     author='Thomas Schmelzer',
     author_email='thomas.schmelzer@lobnek.com',
     description='', install_requires=['pandas>=0.23.4','flask==1.0.2','flask-sqlalchemy==2.3.2','flask-restplus==0.12.1', 'Flask-WTF==0.14.2','flask-caching==1.4.0'],
-    package_data = {'pyweb': ['*.html']},
+    #package_data = {'pyweb': ['*.html']},
+    #packages = ['templates','static'],
+    #package_data={'templates':['*'],'static':['*']}
+
     include_package_data=True
 )
 
