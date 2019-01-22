6496e63e33ec7432f205df73fa1de44ea31b6265
tschm <thomas.schmelzer@gmail.com>
2019-01-22 23:02:07 +0100

    cleaning

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-#http://blog.codekills.net/2011/07/15/lies,-more-lies-and-python-packaging-documentation-on--package_data-/
+# http://blog.codekills.net/2011/07/15/lies,-more-lies-and-python-packaging-documentation-on--package_data-/
 
 setup(
     name='pyweb',
     version="0.0.1",
     package_dir={'pyweb': "pyweb"},
     packages=find_packages(include=["pyweb*"]),
-    package_data={'pyweb':['pyweb/blueprints/**/templates/**', 'pyweb/blueprints/**/static/**']},
+    package_data={'pyweb': ['pyweb/blueprints/**/templates/**', 'pyweb/blueprints/**/static/**']},
     include_package_data=True,
     author='Thomas Schmelzer',
     author_email='thomas.schmelzer@lobnek.com',
-    description='', install_requires=['pandas>=0.23.4','flask==1.0.2','flask-sqlalchemy==2.3.2', 'Flask-WTF==0.14.2','flask-caching==1.4.0'],
+    description='',
+    install_requires=['pandas>=0.23.4', 'flask==1.0.2', 'flask-sqlalchemy==2.3.2', 'Flask-WTF==0.14.2',
+                      'flask-caching==1.4.0']
 )
-
-
