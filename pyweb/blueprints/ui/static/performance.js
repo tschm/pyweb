d591bea8a258abfbb60a284073ce45a7cf693632
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-01-22 16:01:41 +0100

    package data

---
+++ pyweb/blueprints/ui/static/performance.js
@@ -0,0 +1,59 @@
+lobnekperf = {
+    parse: function (x) {
+        rows = [];
+        for (var key in x) {
+            rows.push({"name": key, "value": x[key]});
+        }
+        return rows;
+    },
+
+    truncate: function (s) {
+        var n = s.xData.length;
+        var pairs = [];
+
+        for (i = 0; i < n; i++) {
+            if (s.xData[i] >= s.xAxis.getExtremes().min && s.xData[i] <= s.xAxis.getExtremes().max) {
+                pairs.push([s.xData[i], s.yData[i]])
+            }
+        }
+
+        return pairs;
+    },
+
+    performance: function (s) {
+        return $.ajax({
+            url: "/api/1/engine/performance",
+            type: "POST",
+            data: JSON.stringify(lobnekperf.truncate(s)),
+            contentType: "application/json; charset=utf-8"
+        });
+    },
+
+
+    monthtable: function (s) {
+        return $.ajax({
+            url: "/api/1/engine/month",
+            type: "POST",
+            data: JSON.stringify(s),
+            contentType: "application/json; charset=utf-8"
+        });
+    },
+
+    drawdown: function (s) {
+        return $.ajax({
+            url: "/api/1/engine/drawdown",
+            type: "POST",
+            data: JSON.stringify(s),
+            contentType: "application/json; charset=utf-8",
+        });
+    },
+
+    volatility: function (s) {
+        return $.ajax({
+            url: "/api/1/engine/volatility",
+            type: "POST",
+            data: JSON.stringify(s),
+            contentType: "application/json; charset=utf-8",
+        });
+    }
+};
