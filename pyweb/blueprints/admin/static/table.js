cf1e9dc1c6dc9fd6a15d0a07d82f67eee89661d2
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-01-23 13:26:49 +0100

    starting pyweb

--- pyweb/blueprints/admin/static/table.js
+++ pyweb/blueprints/admin/static/table.js
@@ -35,3 +35,20 @@ lobnektable = {
         })
     },
 }
+
+lobnekgraph = {
+    rangeselector: {
+        buttons: [
+            {type: 'ytd', text: 'YTD'},
+            {type: 'year', count: 1, text: '1y'},
+            {type: 'year', count: 2, text: '2y'},
+            {type: 'year', count: 3, text: '3y'},
+            {type: 'year', count: 4, text: '4y'},
+            {type: 'year', count: 5, text: '5y'},
+            {type: 'year', count: 6, text: '6y'},
+            {type: 'year', count: 10, text: '10y'},
+            {type: 'all', text: 'All'}
+        ],
+        selected: 8
+    }
+};
