99f1c985812e1873d0b5ceb4f4c131c9b3852459
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-01-24 17:49:43 +0100

    javascript

--- pyweb/exts/exts.py
+++ pyweb/exts/exts.py
@@ -1,7 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
-#from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 cache = Cache()
-#csrf_protect = CSRFProtect()
