fc8f501d6fa27057fd6b89cd6aad8823ff73a1bc
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-03-29 12:07:20 +0100

    update docker

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker-compose build --no-cache test
-docker-compose run test
+docker-compose -f docker-compose.test.yml build --no-cache test
+docker-compose -f docker-compose.test.yml run test
 #docker-compose -f docker-compose.test.yml run test npm test
