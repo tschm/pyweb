5116576575b06da3c10aa6eb45bb4693d4c92bd9
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-04-04 10:28:57 +0200

    update docker

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker-compose build jupyter
-docker-compose build web
-
-docker-compose -f docker-compose.test.yml build test
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "http://localhost:4445/admin"
-docker-compose run -p "4445:8000" web
-
