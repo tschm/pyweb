e753360bce6de61d53f91b4581096f12684c2b5c
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-08-20 14:34:09 +0200

    update requirements

--- pyweb/application.py
+++ pyweb/application.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+from pyweb.blueprints.whoosh.api import blueprint as blue_whoosh
+from pyweb.blueprints.post.api import blueprint as blue_post
+from pyweb.blueprints.admin.api import blueprint as blue_ui
 
 def create_app(static_folder="static", template_folder="templates", extensions=None):
     app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
@@ -9,6 +12,11 @@ def create_app(static_folder="static", template_folder="templates", extensions=N
 
     for extension in extensions:
         extension.init_app(app)
+
+    app.register_blueprint(blue_whoosh, url_prefix="/api/1/whoosh")
+    app.register_blueprint(blue_post, url_prefix="/api/1/engine")
+    app.register_blueprint(blue_ui, url_prefix="/admin")
+
 
     return app
 
--- start.py
+++ start.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 from waitress import serve
-
 from pyweb.application import create_app
-
 from pyweb.exts.exts import db
-
-from pyweb.blueprints.whoosh.api import blueprint as blue_whoosh
-from pyweb.blueprints.post.api import blueprint as blue_post
-from pyweb.blueprints.admin.api import blueprint as blue_ui
 
 
 if __name__ == '__main__':
@@ -17,10 +11,4 @@ if __name__ == '__main__':
 
     # This assert fails if we start with docker-compose.test.yml...
     # assert not app.config["TESTING"]
-    app = create_app(extensions=[db])
-
-    app.register_blueprint(blue_whoosh, url_prefix="/api/1/whoosh")
-    app.register_blueprint(blue_post, url_prefix="/api/1/engine")
-    app.register_blueprint(blue_ui, url_prefix="/admin")
-
-    serve(app=app, port=8000)
+    serve(app=create_app(extensions=[db]), port=8000)
