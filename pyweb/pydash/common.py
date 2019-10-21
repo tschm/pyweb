e85ff0dea37b0e1e01c98572fc6694c23e760775
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-10-21 11:58:49 +0200

    href

--- pyweb/pydash/common.py
+++ pyweb/pydash/common.py
@@ -1,13 +1,20 @@
 # Meta tags for viewport responsiveness
 from abc import ABCMeta, abstractmethod
+from collections import namedtuple
 
 from flask.helpers import get_root_path
-
-from pyweb.pydash.pydash.dash_util import *
 
 style = {'width': '96%', 'display': 'inline-block', 'padding': 10}
 
 AppTuple = namedtuple('App', ['dash', 'href', 'text'])
+
+# don't delete; every app can use this functionality
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_daq as daq
+from dash.dependencies import Output, Input, State
+import dash_table
+
 
 
 class App(object):
