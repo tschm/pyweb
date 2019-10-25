14ef2492ab566be9f08ff3a4561ce685f98add45
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-10-25 09:49:28 +0200

    doc problem

--- pyweb/application.py
+++ pyweb/application.py
@@ -8,14 +8,7 @@ from pyweb.blueprints.admin.api import construct_navbar, LinkTuple
 def create_app(server=None, static_folder="static", template_folder="templates", extensions=None):
     server = server or create_server(static_folder=static_folder, template_folder=template_folder, extensions=extensions)
 
-    #app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
-    #success = app.config.from_envvar('APPLICATION_SETTINGS', silent=False)
-    #assert success
-
     with server.test_request_context():
-        #for extension in extensions:
-        #    extension.init_app(current_app)
-
         current_app.register_blueprint(blue_whoosh, url_prefix="/api/1/whoosh")
         current_app.register_blueprint(blue_post, url_prefix="/api/1/engine")
 
