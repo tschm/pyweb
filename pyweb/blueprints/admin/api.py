6932e107ea61b764b3331765a75fdef32a5641a0
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-11-01 15:50:11 +0100

    Version in bar

--- pyweb/blueprints/admin/api.py
+++ pyweb/blueprints/admin/api.py
@@ -6,7 +6,6 @@ LinkTuple = namedtuple('Link', ['href', 'text'])
 
 def construct_navbar(links=None, version=None):
     blueprint = Blueprint('admin', __name__, template_folder="templates", static_folder="static")
-    version = version or 0
     @blueprint.route('', methods=['GET'])
     def get_index():
         return render_template("index.html", links=links, version=version)
