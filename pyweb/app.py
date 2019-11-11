f189ff5829e15237ea3462a7b2e17b788be56f06
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-11-11 12:47:13 +0100

    Version in bar

--- pyweb/app.py
+++ pyweb/app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import os
 
-from flask import current_app
+from flask import current_app, url_for
 
 from pyweb.application import create_server
-from pyweb.blueprints.admin.api import construct_navbar
+from pyweb.blueprints.admin.api import construct_navbar, LinkTuple
 from pyweb.exts.exts import db
 from pyweb import __verion__ as version
+from pyweb.blueprints.whoosh.api import blueprint as blue_whoosh
 
 base_dir = os.path.dirname(__file__)
 
@@ -17,10 +18,15 @@ def create_app():
                            extensions=[db])
 
     with server.test_request_context():
-        #print(url_for("whoosh_api.search", fmt="html"))
+        # register the blueprint
+        current_app.register_blueprint(blue_whoosh, url_prefix="/api/1/whoosh")
 
-        #links = [LinkTuple(href=url_for("whoosh_api.search", fmt="html"), text="Search")]
-        current_app.register_blueprint(construct_navbar(version=version), url_prefix="/admin")
+        # you have to register the blueprint before you use the url_for...
+        print(url_for("whoosh_api.search", fmt="html"))
+
+        # links
+        links = [LinkTuple(href=url_for("whoosh_api.search", fmt="html"), text="Search")]
+        current_app.register_blueprint(construct_navbar(links=links, version=version), url_prefix="/admin")
 
     return server
 
