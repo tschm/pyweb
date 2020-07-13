8285ca386223d6209c2a2a3a14e4a9d19660ae13
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-07-13 11:03:27 +0200

    removing the menu/jinja2 construct

--- pyweb/app.py
+++ pyweb/app.py
@@ -1,8 +1,10 @@
 from pyutil.web.application import create_server
 from pyweb.blueprints.whoosh.api import blueprint as blue_whoosh
 from pyweb.blueprints.post.api import blueprint as blue_post
-from pyweb.blueprints.admin.api import blueprint as blue_admin
+from pyweb.blueprints.admin.api import blue_admin
 from pyweb.exts.exts import engine, cache
+
+from pyweb import __version__ as version
 from whitenoise import WhiteNoise
 
 
@@ -11,7 +13,7 @@ def create_app():
 
     server.register_blueprint(blue_whoosh, url_prefix="/whoosh")
     server.register_blueprint(blue_post, url_prefix="/engine")
-    server.register_blueprint(blue_admin, url_prefix="/admin")
+    server.register_blueprint(blue_admin(version), url_prefix="/admin")
 
     # add whitenoise
     server.wsgi_app = WhiteNoise(server.wsgi_app, root='/static', prefix='assets/')
--- pyweb/blueprints/admin/api.py
+++ pyweb/blueprints/admin/api.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
-from pyweb import __version__ as version
 
-blueprint = Blueprint('admin', __name__, template_folder="templates", static_folder=None)
+def blue_admin(version):
+    blueprint = Blueprint('admin', __name__, template_folder="templates", static_folder=None)
 
-# You can override the templates... with the central templates of the application
-@blueprint.route('', methods=['GET'])
-def get_index():
-    return render_template("index.html", version=version)
+    # You can override the templates... with the central templates of the application
+    @blueprint.route('', methods=['GET'])
+    def get_index():
+        return render_template("index.html", version=version)
+
+    return blueprint
