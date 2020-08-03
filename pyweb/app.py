e5d3d61ba56ca7d0d7ad373488916cc2c646ea08
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-08-03 18:42:49 +0200

    flask bootstrap

--- pyweb/app.py
+++ pyweb/app.py
@@ -4,7 +4,7 @@ from pyweb.blueprints.whoosh.api import blueprint as blue_whoosh
 from pyweb.blueprints.post.api import blueprint as blue_post
 from pyweb.blueprints.admin.api import blue_admin
 
-from pyweb.exts.exts import engine, cache
+from pyweb.exts.exts import engine, cache, bootstrap
 
 from pyweb import __version__ as version
 
@@ -12,7 +12,7 @@ from whitenoise import WhiteNoise
 
 
 def create_app():
-    server = create_server(extensions=[engine, cache], static_folder="/static")
+    server = create_server(extensions=[engine, cache, bootstrap], static_folder="/static")
 
     server.register_blueprint(blue_whoosh, url_prefix="/whoosh")
     server.register_blueprint(blue_post, url_prefix="/engine")
--- pyweb/exts/exts.py
+++ pyweb/exts/exts.py
@@ -1,7 +1,8 @@
 from flask_caching import Cache
 from flask_mongoengine import MongoEngine
+from flask_bootstrap import Bootstrap
 
 cache = Cache()
 engine = MongoEngine()
-
+bootstrap = Bootstrap()
 
