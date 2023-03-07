34b95f0ac6167ae382e04ddd3bae1daf98d50a5f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-03-07 23:16:09 +0400

    Update requests.py

--- pyweb/web/requests.py
+++ pyweb/web/requests.py
@@ -7,14 +7,12 @@ import requests
 def fetch_csv(url, params=None, **kwargs):
     r = requests.get(url, params)
     if r.status_code != 200:
-        raise AssertionError(
-            "The status code is {status}".format(status=r.status_code))
+        raise AssertionError(f"The status code is {r.status_code}")
     return pd.read_csv(io.StringIO(r.content.decode()), **kwargs)
 
 
 def fetch_json(url, params=None, **kwargs):
     r = requests.get(url, params)
     if not r.ok:
-        raise AssertionError(
-            "The status code is {status}".format(status=r.status_code))
+        raise AssertionError(f"The status code is {r.status_code}")
     return pd.read_json(r.json(), **kwargs)
