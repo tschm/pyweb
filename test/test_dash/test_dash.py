9df1a773844335c7ff856d9c444644d0ce97ba10
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-03-07 23:12:47 +0400

    testing

--- test/test_dash/test_dash.py
+++ test/test_dash/test_dash.py
@@ -37,90 +37,88 @@ class MyApp4(App):
         super().__init__(**kwargs)
 
 
-class TestDash:
-    def test_cache(self):
-        x = MyApp(name="maffay")
-        assert x.register_callback() == 2
+def test_cache():
+    x = MyApp(name="maffay")
+    assert x.register_callback() == 2
 
-    def test_logger(self):
-        x = MyApp(name="wurst")
+def test_logger():
+    x = MyApp(name="wurst")
 
-        # there exists an logger
-        assert x.logger
+    # there exists an logger
+    assert x.logger
 
-        # set the level for the logger
-        x.logger.setLevel(logging.DEBUG)
+    # set the level for the logger
+    x.logger.setLevel(logging.DEBUG)
 
-        # no log messages
-        assert x.logs == []
+    # no log messages
+    assert x.logs == []
 
-        x.logs = ["peter"]
-        assert x.logs == ["peter"]
+    x.logs = ["peter"]
+    assert x.logs == ["peter"]
 
-        x.logs = []
-        assert x.logs == []
+    x.logs = []
+    assert x.logs == []
 
-        x.logger.debug("test")
-        assert x.logs[0][-4:] == "test"
+    x.logger.debug("test")
+    assert x.logs[0][-4:] == "test"
 
-    def test_not_implemented(self):
-        with pytest.raises(NotImplementedError):
-            MyApp2(name="wurst")
+def test_not_implemented():
+    with pytest.raises(NotImplementedError):
+        MyApp2(name="wurst")
 
-    def test_not_implemented_layout(self):
-        with pytest.raises(NotImplementedError):
-            x = MyApp3(name="wurst")
-            x.build_layout()
+def test_not_implemented_layout():
+    with pytest.raises(NotImplementedError):
+        x = MyApp3(name="wurst")
+        x.build_layout()
 
-    def test_not_implemented_callback(self):
-        with pytest.raises(NotImplementedError):
-            x = MyApp4(name="wurst")
-            x.register_callback()
+def test_not_implemented_callback():
+    with pytest.raises(NotImplementedError):
+        x = MyApp4(name="wurst")
+        x.register_callback()
 
 
-class TestDash2:
-    def test_cache(self):
-        x = MyApp(name="maffay")
-        assert x.register_callback() == 2
+def test_cache():
+    x = MyApp(name="maffay")
+    assert x.register_callback() == 2
 
-    def test_flask(self):
-        app = Flask(__name__)
-        app.config["Wurst"] = "Peter Maffay"
+def test_flask():
+    app = Flask(__name__)
+    app.config["Wurst"] = "Peter Maffay"
 
-        dash = MyApp.dash_application(url="/admin")
-        dash.init_app(app=app)
+    dash = MyApp.dash_application(url="/admin")
+    dash.init_app(app=app)
 
-    def test_logger(self):
-        x = MyApp(name="wurst")
+def test_logger():
+    x = MyApp(name="wurst")
 
-        # there exists an logger
-        assert x.logger
+    # there exists an logger
+    assert x.logger
 
-        # set the level for the logger
-        x.logger.setLevel(logging.DEBUG)
+    # set the level for the logger
+    x.logger.setLevel(logging.DEBUG)
 
-        # no log messages
-        assert x.logs == []
+    # no log messages
+    assert x.logs == []
 
-        x.logs = ["peter"]
-        assert x.logs == ["peter"]
+    x.logs = ["peter"]
+    assert x.logs == ["peter"]
 
-        x.logs = []
-        assert x.logs == []
+    x.logs = []
+    assert x.logs == []
 
-        x.logger.debug("test")
-        assert x.logs[0][-4:] == "test"
+    x.logger.debug("test")
+    assert x.logs[0][-4:] == "test"
 
-    def test_not_implemented(self):
-        with pytest.raises(NotImplementedError):
-            MyApp2(name="wurst")
+def test_not_implemented():
+    with pytest.raises(NotImplementedError):
+        MyApp2(name="wurst")
 
-    def test_not_implemented_layout(self):
-        with pytest.raises(NotImplementedError):
-            x = MyApp3(name="wurst")
-            x.build_layout()
+def test_not_implemented_layout():
+    with pytest.raises(NotImplementedError):
+        x = MyApp3(name="wurst")
+        x.build_layout()
 
-    def test_not_implemented_callback(self):
-        with pytest.raises(NotImplementedError):
-            x = MyApp4(name="wurst")
-            x.register_callback()
+def test_not_implemented_callback():
+    with pytest.raises(NotImplementedError):
+        x = MyApp4(name="wurst")
+        x.register_callback()
