31f36dac146e9365d95d15dea5e89286daccf2da
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-03-07 23:36:07 +0400

    testing

--- pyweb/testing/response.py
+++ pyweb/testing/response.py
@@ -15,19 +15,19 @@ def get(client, url):
     response = client.get(url)
     if response.status_code != 200:
         raise AssertionError(
-            "The return code is {r}".format(r=response.status_code))
+            f"The return code is {response.status_code}")
     return response
 
 
 def response2csv(response, **kwargs):
     if response.status_code != 200:
         raise AssertionError(
-            "The return code is {r}".format(r=response.status_code))
+            f"The return code is {response.status_code}")
     return pd.read_csv(StringIO(response.data.decode()), **kwargs)
 
 
 def response2json(response, **kwargs):
     if response.status_code != 200:
         raise AssertionError(
-            "The return code is {r}".format(r=response.status_code))
+            f"The return code is {response.status_code}")
     return pd.read_json(response.data.decode(), **kwargs)
