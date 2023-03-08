009692902f4ef233ac507753771b60ee3a5bb7a6
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-03-08 08:12:45 +0400

    formatting f strings

--- pyweb/blueprints/admin/api.py
+++ pyweb/blueprints/admin/api.py
@@ -8,7 +8,7 @@ blueprint = Blueprint(
     "admin", __name__, template_folder="templates", static_folder=None
 )
 
-    # You can override the templates... with the central templates of the application
+
 @blueprint.route("", methods=["GET"])
 def index():
     return render_template("index.html", version=version)
