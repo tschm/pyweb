d892b43e757e40b3087cb6615f7e5038957a4368
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-03-08 08:10:21 +0400

    formatting f strings

--- pyweb/app.py
+++ pyweb/app.py
@@ -1,7 +1,6 @@
 from whitenoise import WhiteNoise
 
-from pyweb import __version__ as version
-from pyweb.blueprints.admin.api import blue_admin
+from pyweb.blueprints.admin.api import blueprint as blue_admin
 from pyweb.blueprints.post.api import blueprint as blue_post
 from pyweb.exts.exts import bootstrap, cache
 from pyweb.web.application import create_server
@@ -13,7 +12,7 @@ def create_app():
     )
 
     server.register_blueprint(blue_post, url_prefix="/engine")
-    server.register_blueprint(blue_admin(version), url_prefix="/admin")
+    server.register_blueprint(blue_admin, url_prefix="/admin")
 
     # add whitenoise
     server.wsgi_app = WhiteNoise(
--- pyweb/blueprints/admin/api.py
+++ pyweb/blueprints/admin/api.py
@@ -1,14 +1,14 @@
+import os
+
 from flask import Blueprint, render_template
 
+version = os.getenv("VERSION", "0.0.0")
 
-def blue_admin(version):
-    blueprint = Blueprint(
-        "admin", __name__, template_folder="templates", static_folder=None
-    )
+blueprint = Blueprint(
+    "admin", __name__, template_folder="templates", static_folder=None
+)
 
     # You can override the templates... with the central templates of the application
-    @blueprint.route("", methods=["GET"])
-    def index():
-        return render_template("index.html", version=version)
-
-    return blueprint
+@blueprint.route("", methods=["GET"])
+def index():
+    return render_template("index.html", version=version)
