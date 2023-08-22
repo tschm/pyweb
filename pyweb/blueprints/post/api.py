c31a097f4e43fe6501d176c35611ea85a3da3eb5
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-08-21 20:55:52 -0700

    perf code

--- pyweb/blueprints/post/api.py
+++ pyweb/blueprints/post/api.py
@@ -1,42 +1,25 @@
 import json
 
 from flask import Blueprint, request
-from pyutil.performance.return_series import from_nav
 
-from pyweb.web.highcharts import parse, to_json
+from pyweb.web.highcharts import parse
 from pyweb.web.parser import respond_pandas
 
 blueprint = Blueprint("post", __name__, static_folder="static")
 
 
-def __percentage(x):
-    return f"{100 * float(x):.2f}%".replace("nan%", "")
-
-
 def __series():
-    return from_nav(parse(value=json.loads(request.data)))
+    return parse(value=json.loads(request.data))
     # series is an array [[t1,v1],[t2,v2],...]
 
 
 @blueprint.route("/performance", methods=["POST"])
 def performance():
-    perf = __series().summary_format().apply(str)
-    return respond_pandas(frame=perf.to_dict(), fmt="json")
+    returns = __series().pct_change()
+    perf = returns.describe()
+    perf = {key: f"{value:.2%}" for key, value in perf.to_dict().items()}
+    perf["count"] = str(len(__series()))
+    # print(perf.to_dict())
+    print(perf)
 
-
-@blueprint.route("/month", methods=["POST"])
-def month():
-    # return a frame...
-    return respond_pandas(
-        frame=__series().monthlytable.applymap(__percentage), fmt="json"
-    )
-
-
-@blueprint.route("/drawdown", methods=["POST"])
-def drawdown():
-    return respond_pandas(frame=to_json(__series().drawdown), fmt="json")
-
-
-@blueprint.route("/volatility", methods=["POST"])
-def volatility():
-    return respond_pandas(frame=to_json(__series().ewm_volatility()), fmt="json")
+    return respond_pandas(frame=perf, fmt="json")
--- tests/test_apis/test_post.py
+++ tests/test_apis/test_post.py
@@ -1,12 +1,10 @@
 import json
 
 import pandas as pd
-import pandas.testing as pt
 import pytest
-from pyutil.performance.return_series import from_nav
 
 from pyweb.testing.response import post
-from pyweb.web.highcharts import parse, to_json
+from pyweb.web.highcharts import to_json
 
 
 @pytest.fixture(scope="module")
@@ -25,59 +23,17 @@ def data(price):
     return json.dumps(data)
 
 
-def test_drawdown(client, data, price):
-    d = json.loads(post(client=client, data=data, url="/engine/drawdown").data.decode())
-    x = parse(d)
-    pt.assert_series_equal(from_nav(price).drawdown, x, check_names=False)
-
-
-def test_volatility(client, data, price):
-    d = json.loads(
-        post(client=client, data=data, url="/engine/volatility").data.decode()
-    )
-    x = parse(d)
-    pt.assert_series_equal(
-        from_nav(price).ewm_volatility().dropna(), x, check_names=False
-    )
-
-
-def test_month(client, data, price):
-    d = post(client=client, data=data, url="/engine/month").data.decode()
-    x = pd.read_json(d, orient="table")
-    pt.assert_frame_equal(
-        x,
-        from_nav(price).monthlytable.applymap(
-            lambda x: f"{float(100.0 * x):.2f}%".replace("nan%", "")
-        ),
-        check_names=False,
-    )
-
-
 def test_performance(client, data):
     d = post(client=client, data=data, url="/engine/performance").data.decode()
     x = pd.read_json(d, typ="series")
-
+    print(x)
     assert x.to_dict() == {
-        "Return": "-28.27%",
-        "# Events": "602",
-        "# Events per year": "261",
-        "Annua Return": "-14.40%",
-        "Annua Volatility": "18.01%",
-        "Annua Sharpe Ratio (r_f = 0)": "-0.80",
-        "Max Drawdown": "32.61%",
-        "Max % return": "4.07%",
-        "Min % return": "-9.11%",
-        "MTD": "1.43%",
-        "YTD": "1.33%",
-        "Current Nav": "1200.59",
-        "Max Nav": "1692.70",
-        "Current Drawdown": "29.07%",
-        "Calmar Ratio (3Y)": "-0.44",
-        "# Positive Events": "286",
-        "# Negative Events": "316",
-        "Value at Risk (alpha = 95)": "1.91%",
-        "Conditional Value at Risk (alpha = 95)": "2.76%",
-        "First at": "2013-01-01",
-        "Last at": "2015-04-22",
-        "Kurtosis": "8.75",
+        "count": "602",
+        "mean": "-0.05%",
+        "std": "1.12%",
+        "min": "-9.11%",
+        "25%": "-0.52%",
+        "50%": "-0.04%",
+        "75%": "0.59%",
+        "max": "4.07%",
     }
