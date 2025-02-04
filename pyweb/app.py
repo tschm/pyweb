68d2b4acf10fe8db95bd628b1e0af849ae0a54f5
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-04 07:01:21 +0400

    static into src folder

--- pyweb/app.py
+++ pyweb/app.py
@@ -7,7 +7,7 @@ from pyweb.web.application import create_server
 
 
 def create_app():
-    server = create_server(extensions=[cache, bootstrap], static_folder="/static")
+    server = create_server(extensions=[cache, bootstrap], static_folder="static")
 
     server.register_blueprint(blue_post, url_prefix="/engine")
     server.register_blueprint(blue_admin, url_prefix="/admin")
